7f8f8fe3226c0003bd14210e917f49dda6b2de7b
shan2202 <shanswamy@gmail.com>
2024-08-06 20:10:17 +0800

    update security scan and pipeline scripts

--- terraform/prerequisite.sh
+++ terraform/prerequisite.sh
@@ -2,8 +2,9 @@
 BUCKETNAME="872132143780-tfstate-bucket"
 REGION="ap-southeast-1"
 DDBTABLENAME="tfstate-table"
+PROJECT_NAME="webgoat"
 #Create S3 bucket to store tfstate file with versioning enbaled.
-aws s3api create-bucket --bucket $REGION-$BUCKETNAME --region $REGION --create-bucket-configuration LocationConstraint=$REGION
-aws s3api put-bucket-versioning --bucket $REGION-$BUCKETNAME --versioning-configuration Status=Enabled
+aws s3api create-bucket --bucket $PROJECT_NAME-$REGION-$BUCKETNAME --region $REGION --create-bucket-configuration LocationConstraint=$REGION
+aws s3api put-bucket-versioning --bucket $PROJECT_NAME-$REGION-$BUCKETNAME --versioning-configuration Status=Enabled
 
-aws dynamodb create-table --table-name $REGION-$DDBTABLENAME --attribute-definitions AttributeName=LockID,AttributeType=S --key-schema AttributeName=LockID,KeyType=HASH --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5 --region $REGION 
+aws dynamodb create-table --table-name $PROJECT_NAME-$REGION-$DDBTABLENAME --attribute-definitions AttributeName=LockID,AttributeType=S --key-schema AttributeName=LockID,KeyType=HASH --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5 --region $REGION 
